204ea3c086beacb7050a9b424df4a992c180f2e1
Yuki Yasoshima <yukiyasos@gmail.com>
2016-05-07 16:10:46 +0900

    add ios_class_tests

---
+++ objc_utils_test/objc_utils_test_ios_non_arc/yas_objc_ios_class_tests.m
@@ -0,0 +1,28 @@
+//
+//  yas_objc_ios_class_tests.m
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_objc_macros.h"
+
+@interface yas_objc_ios_class_tests : XCTestCase
+
+@end
+
+@implementation yas_objc_ios_class_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_class {
+    XCTAssertEqualObjects([yas_objc_view class], [UIView class]);
+    XCTAssertEqualObjects([yas_objc_view_controller class], [UIViewController class]);
+    XCTAssertEqualObjects([yas_objc_color class], [UIColor class]);
+}
+
+@end
